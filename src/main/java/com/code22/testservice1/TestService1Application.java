cc2e0f3be475e1215d4b291493da198fbf3e3ac4
Prateek Kishore Karnad <trainer.it.ssk@gmail.com>
2022-07-28 16:58:05 +0530

    @RestController annotation added. URI designed properly

--- src/main/java/com/code22/testservice1/TestService1Application.java
+++ src/main/java/com/code22/testservice1/TestService1Application.java
@@ -1,27 +1,27 @@
 package com.code22.testservice1;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/0.1/test-service-1")
 public class TestService1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(TestService1Application.class, args);
     }
 
-    @GetMapping(value = "/test", produces = "application/json")
-    public String getResponse() throws JsonProcessingException {
+    @GetMapping(value="/health", produces = "application/json")
+    public Message getHealth()   {
         Message testMsg = new Message();
-        testMsg.setMsgString("Everything A.OK!");
-        ObjectMapper objectMapper = new ObjectMapper();
-        return  objectMapper.writeValueAsString(testMsg);
+        testMsg.setMsgString("Health A.OK!");
+        //ObjectMapper objectMapper = new ObjectMapper();
+        return  testMsg;
 
     }
 
